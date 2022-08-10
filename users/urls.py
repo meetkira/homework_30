0af06ef13555ecdfa1ec9d19b552c728cd02a825
Кристина Ратник <kristi.2001@yandex.ru>
2022-08-10 11:38:58 +0300

    add token

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users import views
 
@@ -10,4 +11,6 @@ urlpatterns = [
     path("create/", views.UserCreateView.as_view(), name='create_user'),
     path("<int:pk>/update/", views.UserUpdateView.as_view(), name='update_user'),
     path("<int:pk>/delete/", views.UserDeleteView.as_view(), name='delete_user'),
+    path("token/", TokenObtainPairView.as_view(), name='token'),
+    path("token/refresh/", TokenRefreshView.as_view(), name='refresh_token'),
 ]
