88734ebb81005964051d4bda22e7ee7ddc1cb404
Кристина Ратник <kristi.2001@yandex.ru>
2022-08-10 11:24:23 +0300

    Initial commit

---
+++ users/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    lat = models.FloatField(null=True, blank=True)
+    lng = models.FloatField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Расположение"
+        verbose_name_plural = "Расположения"
+
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
+    ROLE = [
+        ("member", "Пользователь"),
+        ("moderator", "Модератор"),
+        ("admin", "Админ"),
+    ]
+    role = models.CharField(max_length=9, default="member", choices=ROLE)
+    age = models.PositiveIntegerField()
+    locations = models.ManyToManyField(Location)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["username"]
+
+    def __str__(self):
+        return self.username
---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+
+from users import views
+
+urlpatterns = [
+    path('', views.index),
+    path('user/', views.UserListView.as_view(), name='users'),
+    path("user/<int:pk>/", views.UserDetailView.as_view(), name='user'),
+    path("create/", views.UserCreateView.as_view(), name='create_user'),
+    path("<int:pk>/update/", views.UserUpdateView.as_view(), name='update_user'),
+    path("<int:pk>/delete/", views.UserDeleteView.as_view(), name='delete_user'),
+]
